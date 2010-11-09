f469427ec108680715244f7b9ec8166c41803edb
Sunil Menon <sunil@nexleaf.org>
2010-11-08 16:55:14 -0800

    adding changes for persistance to all of the remaining tasks

--- taskmanager/tasks/appointment_followup.py
+++ taskmanager/tasks/appointment_followup.py
@@ -10,9 +10,14 @@ from django.template.loader import render_to_string
 from taskmanager.models import *
 import taskscheduler
 
-class AppointmentFollowup(object):
+from task import Task
+
+class AppointmentFollowup(Task):
+    
     def __init__(self, user, args=None):
 
+        Task.__init__(self)
+        
         self.args = args
         print 'in appointmentrequest: self.args: %s; type(self.args):%s' % (self.args, type(self.args))
         
@@ -22,7 +27,6 @@ class AppointmentFollowup(object):
         else:
             raise ValueError('unknown type given for user: %s' % user)
 
-
         # m1
         # resolves to:
         # How was your {{ args.appt_type }}? Reply with 'comment' and feedback; or a time (10/1/2020 16:30:00) to reschedule.
@@ -30,22 +34,36 @@ class AppointmentFollowup(object):
         r1 = sms.Response('comment', match_regex=r'comment|COMMENT', label='comment', callback=self.store_feedback)
         #r2 = sms.Response('8/30/2010 16:30:00', r'\d+/\d+/\d+\s\d+:\d+:\d+', label='datetime', callback=self.reschedule_reminder)
         r2 = sms.Response('8/30/2010 16:30:00', match_callback=AppointmentFollowup.match_date, label='datetime', callback=self.reschedule_reminder)
-        m1 = sms.Message(q1, [r1,r2])
+        m1 = sms.Message(q1, [r1,r2], label='m1')
 
         # m2
         q2 = 'Thank you for your feedback.'
-        m2 = sms.Message(q2, [])
+        m2 = sms.Message(q2, [], label='m2')
 
         # m3
         q3 = 'Ok, you will be sent a reminder to reschedule as you have requested.'
-        m3 = sms.Message(q3, [])
+        m3 = sms.Message(q3, [], label='m3')
         
         self.graph = { m1: [m2, m3],
                        m2: [],
                        m3: [] }
 
-        self.interaction = sms.Interaction(self.graph, m1, self.__class__.__name__ + '_interaction')
-    
+        # self.interaction = sms.Interaction(self.graph, m1, self.__class__.__name__ + '_interaction')
+        super(AppointmentFollowup, self).setinteraction(graph=self.graph, initialnode=m1, label='interaction')
+
+
+    # developer is required to implement save()
+    def save(self):
+        print 'in %s.save(): ' % (self.__class__.__name__)
+        # save whatever you like in the parameter blob
+        return json.dumps({})
+
+    # developer required to implement restore():
+    def restore(self, pb_str):
+        print 'in %s.restore() stub' % (self.__class__.__name__)
+        pb = json.loads(pb_str)
+
+        
     @staticmethod
     def match_date(msgtxt):
         pdt = parsedatetime.Calendar()
@@ -83,8 +101,6 @@ class AppointmentFollowup(object):
         # get the first 6 values from teh stuct... the rest we do not care about
         t = datetime(*res[0:7])
 
-        #t = datetime.strptime(ndatetime, "%m/%d/%Y %H:%M:%S")
-
         # support cens gui: app_date used to display appointment time only
         #                     Tuesday, 5:30pm, November 03, 2010
         appttime = t.strftime("%A %I:%M%p, %B %d, %Y")
@@ -94,8 +110,7 @@ class AppointmentFollowup(object):
 
         # sched a reminder. 
         d1 = {'task': 'Appointment Reminder','user': self.user.identity,'args': self.args,'schedule_date': t,'session_id': session_id}
-        #d1 = {'task': 'Appointment Reminder','user': self.user.identity,'args': self.args,'schedule_date': t.isoformat,'session_id': session_id}
-
+    
         try:
             taskscheduler.schedule(d1)
         except Exception as e:
