24554b7c579de5d565eaba8af5025426a916a0ef
falquaddoomi <falquaddoomi@ucla.edu>
2011-01-19 13:00:50 -0800

    added SerializedTasks to the admin interface so I can inspect/clear it easily if we run into runtime issues

--- taskmanager/admin.py
+++ taskmanager/admin.py
@@ -16,6 +16,10 @@ admin.site.register(Service)
 admin.site.register(AlertType)
 admin.site.register(Alert)
 
+# admin site visibily for serialzed tasks, as they're some issue here
+from tasks.models import *
+admin.site.register(SerializedTasks)
+
 # admin visibility for scheduled message reminders
 from rapidsms.contrib.scheduler.models import *
 admin.site.register(EventSchedule)
